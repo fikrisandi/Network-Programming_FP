5e4d80ef7eeaad357b8a959ec9af81b33413da5c
Muhammad Fikri Sandi Pratama <mgkiddrock@gmail.com>
2023-06-12 02:31:27 +0700

    Done, okyan join :D

---
+++ client/client.py
@@ -0,0 +1,161 @@
+import socket, time, os
+
+# Constants
+HOST = "localhost"
+PORT = 5000
+BUFFER_SIZE = 1024
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+if client_socket.connect:
+    data = client_socket.recv(1024)
+    data = data.decode()
+    while True:
+        command = input("Input your command > ")
+        cmd = command.split()[0]
+
+        if cmd == "USER":
+            username = input("Input username: ")
+            cmduser = 'USER {}'.format(username)
+            client_socket.send(cmduser.encode())
+            user_msg = client_socket.recv(4096)
+            print(user_msg.decode())
+            
+            password = input("Input password: ")
+            cmdpass = 'PASS {}'.format(password).encode()
+            client_socket.send(cmdpass)
+            user_msg = client_socket.recv(1024)
+            print(user_msg.decode())
+        
+        elif cmd == "CWD":
+            foldername = input("Input directory name to be changed: ")
+            command = ('CWD {}'.format(foldername).encode())
+            client_socket.send(command)
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+        
+        elif cmd == "QUIT":
+            client_socket.send(cmd.encode())
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+            break
+        
+        elif cmd == "PWD":
+            client_socket.send(cmd.encode())
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+
+        elif cmd == "HELP":
+            client_socket.send(cmd.encode())
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+
+        elif cmd == "LIST":
+            client_socket.send(cmd.encode())
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+        
+        elif cmd == "MKD":
+            foldername = input("Input directory name to be created: ")
+            command = ('MKD {}'.format(foldername).encode())
+            client_socket.send(command)
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+        
+        elif cmd == "RMD":
+            foldername = input("Input directory name to be removed: ")
+            command = ('RMD {}'.format(foldername).encode())
+            client_socket.send(command)
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+
+        elif cmd == "STOR":
+            fn = input("Input file name to be uploaded: ")
+            command = ("STOR {}".format(fn))
+            client_socket.send(command.encode())
+
+            file_name = command.split()[1]
+            local = os.getcwd()
+            upload = os.path.join(local, file_name)
+            check = os.path.isfile(upload)
+            file_size = os.path.getsize(upload)
+
+            client_socket.send(str(check).encode())
+            client_socket.send(str(file_size).encode())
+
+            if check:
+                user_msg = client_socket.recv(4096)
+                print(user_msg.decode())
+ 
+                with open(upload, 'rb') as f:
+                    data = f.read()
+                    print(f.read())
+                client_socket.sendall(data)
+
+            user_msg = client_socket.recv(4096)
+            print(user_msg.decode())
+
+        elif cmd == 'RETR':
+            fn = input("Input file name to be downloaded: ")
+            command = ("RETR {}".format(fn))
+            client_socket.send(command.encode())
+
+            file_name = command.split()[1]
+            local = os.getcwd()
+            download = os.path.join(local, file_name)
+            check = client_socket.recv(4).decode()
+            file_size = int(client_socket.recv(BUFFER_SIZE).decode())
+
+            data=""
+            
+            if check == 'True':
+                print("masuk")
+                accepted = ''.encode()
+
+                with open(download, 'wb') as file:
+                    while file_size :
+                        data = client_socket.recv(BUFFER_SIZE*4)
+                        # print(data)
+                        if not data:
+                            break
+                        accepted += data
+                        file.write(data)
+                        file_size -= len(data)
+                        
+            user_msg = client_socket.recv(BUFFER_SIZE*4)
+            print(user_msg.strip().decode())
+
+        elif cmd == 'RNTO':
+            fn = input("Input new file or directory name: ")
+            command = ("RNTO {}".format(fn).encode())
+
+            client_socket.send(command)
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+        
+        elif cmd == 'RNFR':
+            fn = input("Input file or directory name to be renamed: ")
+            command = ("RNFR {}".format(fn).encode())
+
+            client_socket.send(command)
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+
+            if '350' in user_msg.decode():
+                new = input("Input new file or directory name: ")
+                command = ("RNTO {}".format(new).encode())
+                client_socket.send(command)
+            
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+        
+        elif cmd == "DELE":
+            file_name = input("Input file name to be deleted: ")
+            command = ('DELE {}'.format(file_name).encode())
+            client_socket.send(command)
+            user_msg = client_socket.recv(10000)
+            print(user_msg.decode())
+                        
+
+client_socket.close()
---
+++ server/menus.py
@@ -0,0 +1,246 @@
+import os
+
+def fquit(self):
+        self.login_message = "221 Service closing control connection.\r\n"
+        self.conn.send(self.login_message.encode())
+        print("Respon: " + self.login_message.strip())
+        print(self.conn.getpeername())
+        print("\n")
+        self.conn.close()
+    
+def fcwd(self, cmd):
+        chwd = cmd.split()[1]
+        if chwd == '/':
+            self.cwd = self.workdir
+        elif chwd[0] == '/':
+            self.cwd = os.path.join(self.workdir, cmd.split()[1])
+        else:
+            self.cwd = os.path.join(self.cwd, chwd)
+        self.conn.send('250 Requested file action okay, completed.\r\n'.encode())
+        self.login_menu()
+    
+def retr(self, cmd):
+        file_name = cmd.split()[1]
+        path = os.path.join(self.cwd, file_name)
+        check = os.path.isfile(path)
+        file_size = os.path.getsize(path)
+        print('Download: ', file_name)
+        self.conn.send(str(check).encode())
+        self.conn.send(str(file_size).encode())
+        data = ""
+
+        if check:
+            with open(path, 'rb') as f:
+                data = f.read()      
+            self.conn.sendall(data)
+            
+            self.message = '226 Closing data connection. Requested file action successful\r\n'
+            # print("Response: " + self.message.strip())
+            self.conn.send(self.message.encode())
+            print(self.conn.getpeername())
+            print("Response: " + self.message.strip())
+        else:
+            self.message = "501 Syntax error in parameters or arguments.\r\n"
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        self.login_menu()
+
+def rnto(self, cmd):
+        file_name = cmd.split()[1]
+        path = os.path.join(self.cwd, file_name)
+        check = os.path.isfile(path)
+        self.conn.send(str(check).encode())
+        if check:
+            self.message = "350 Ready for RNTO\r\n"
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+            cmd = (self.conn.recv(self.size)).decode()
+            if 'RNTO' in cmd:
+                print("Command: " + cmd.strip())
+                print(self.conn.getpeername())
+                name = os.path.join(self.cwd, cmd.strip().split(' ')[1]) 
+                print(name)
+                os.rename(path, name)
+                self.message = '250 Requested file action okay, completed.\r\n'
+                print('Response: ' + self.message.strip())
+                print(self.conn.getpeername())
+                self.conn.send(self.message.encode())
+            else:
+                self.message = '501 Syntax error in parameters or arguments.\r\n'
+                print('Response: ' + self.message.strip())
+                print(self.conn.getpeername())
+                self.conn.send(self.message.encode())
+        else:
+            self.message = "501 Syntax error in parameters or arguments.\r\n"
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        self.login_menu()    
+
+def rnfr(self, cmd):
+        file_name = cmd.split()[1]
+        path = os.path.join(self.cwd, file_name)
+        checkDir = os.path.isdir(path)
+        checkFile = os.path.isfile(path)
+        print(path)
+
+        if checkDir:
+            self.message = "350 Ready for RNTO directory\r\n"
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+            cmd = (self.conn.recv(self.size)).decode()
+            if 'RNTO' in cmd:
+                print("Command: " + cmd.strip())
+                print(self.conn.getpeername())
+                name = os.path.join(self.cwd, cmd.strip().split(' ')[1]) 
+                print(name)
+                os.rename(path, name)
+                self.message = '250 Requested file action okay, completed.\r\n'
+                print('Response: ' + self.message.strip())
+                print(self.conn.getpeername())
+                self.conn.send(self.message.encode())
+            else:
+                self.message = '501 Syntax error in parameters or arguments.\r\n'
+                print('Response: ' + self.message.strip())
+                print(self.conn.getpeername())
+                self.conn.send(self.message.encode())
+            self.login_menu()
+        elif checkFile:
+            self.message = '350 Ready for RNTO file\r\n'
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+            cmd = (self.conn.recv(self.size)).decode()
+            if 'RNTO' in cmd:
+                print("Command: " + self.message.strip())
+                print(self.conn.getpeername())
+                name = os.path.join(self.cwd, cmd.strip().split(' ')[1])
+                os.rename(path, name)
+                self.message = '250 Rename was successful.\r\n'
+                print("Response: " + self.message.strip())
+                print(self.conn.getpeername())
+                self.conn.send(self.message.encode())
+            else:
+                self.message = '501 Syntax error in parameters or arguments. This usually results from an invalid or missing file name.\r\n'
+                print("Response: " + self.message.strip())
+                print(self.conn.getpeername())
+                self.conn.send(self.message.encode())
+            self.login_menu()
+        else:
+            self.message = '501 Unknown command\r\n'
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+
+def dele(self, cmd):
+        self.chwd = cmd.split()[1]
+        file_name = os.path.join(self.cwd, self.chwd)
+        self.allow_delete = os.path.isfile(file_name)
+        if self.allow_delete:
+            os.remove(file_name)
+            self.message = '250 Requested file action okay, completed.\r\n'
+            print('Response: ' + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        else:
+            self.conn.send('450 Requested file action not taken.\nFile unavailable (e.g., file busy).\r\n'.encode())  
+        self.login_menu()
+
+def rmd(self, cmd):
+        self.chwd = cmd.split()[1]
+        dir_name = os.path.join(self.cwd, self.chwd)
+        self.allow_delete = os.path.isdir(dir_name)
+
+        if self.allow_delete:
+            os.rmdir(dir_name)
+            self.message = '250 Requested file action okay, completed.\r\n'
+            print('Response: ' + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        else:
+            self.message = '550 Requested action not taken.\nFile unavailable (e.g., file not found, no access).\r\n'
+            print('Response: ' + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        self.login_menu()
+
+def mkd(self, cmd):
+        self.chwd = cmd.split()[1]
+        dir_name = os.path.join(self.cwd, self.chwd)
+        self.allow_make = os.path.isdir(dir_name)
+        if self.allow_make:
+            self.message = '550 Requested action not taken.\nFile unavailable (e.g., file not found, no access).\r\n'
+            print('Response: ' + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        else:
+            os.mkdir(dir_name)
+            self.message = '250 Requested file action okay, completed.\r\n'
+            print('Response: ' + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())  
+        self.login_menu()
+
+def pwd(self, cmd):
+        cwd = os.path.relpath(self.cwd, self.workdir)
+        if cwd == '.':
+            cwd = '/'
+        else:
+            cwd = '/'+cwd
+        self.message = '257 ' + self.cwd + ' is current directory.\r\n'
+        print('Response: ' + self.message.strip())
+        print(self.conn.getpeername())
+        self.conn.send(self.message.encode())
+        self.login_menu()
+    
+def flist(self, cmd):
+        message = ('150 File status okay; about to open data connection.\n')
+        dirList = os.listdir(self.cwd)
+        for x in dirList:
+            temp = os.path.join(self.cwd, x)
+            message = message + temp + '\n'
+        self.conn.send(message.encode())
+        self.conn.send('226 Closing data connection.\nRequested file action successful (for example, file transfer or file abort).'.encode())
+        self.login_menu()
+
+def stor(self, cmd):
+        print("masuk stor")
+        file_name = cmd.split()[1]
+        path = os.path.join(self.cwd, file_name)
+        check = self.conn.recv(4).decode()
+        file_size = int(self.conn.recv(self.size).decode())
+        data = ""
+
+        if check == 'True':
+            self.message = '150 File status okay; about to open data connection.\r\n'
+            print("Respon: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+
+            accepted = ''.encode()
+            with open(path, 'wb') as file:
+                while file_size :
+                    data = self.conn.recv(self.size)
+                    if not data:
+                        break
+                    accepted += data
+                    file.write(data)
+                    file_size -= len(data)
+            self.message = '226 Closing data connection.\nRequested file action successful (for example, file transfer or file abort).'
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        else:
+            self.message = '501 Syntax error in parameters or arguments.\r\n'
+            print("Response: " + self.message.strip())
+            print(self.conn.getpeername())
+            self.conn.send(self.message.encode())
+        self.login_menu()    
+
+def fhelp(self, cmd):
+        self.conn.send(
+            '214 The following commands are recognized:\nUSER PASS CWD QUIT RETR STOR RNFR RNTO DELE RMD MKD PWD LIST HELP\r\n'.encode())
+        self.login_menu()
